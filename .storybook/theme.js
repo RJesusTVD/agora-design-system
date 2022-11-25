e695292f93f23ca0340023b2e565f8004d21a71a
CarolineRodrigues <31484196+CarolineRodrigues@users.noreply.github.com>
2022-11-25 10:51:02 +0000

    config

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,7 +4,7 @@ import logo from '../src/assets/logo_ticapp.png';
 export default create({
   base: 'light',
   brandTitle: 'Ágora Design System',
-  brandUrl: 'https://github.com/ticapp/portal-unico__building-blocks__storybook-react',
+  brandUrl: 'https://github.com/ticapp',
   brandImage: logo,
   brandTarget: '_self',
   fontBase: '"Noto Sans", sans-serif'
