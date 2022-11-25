b50d39df1d550d9888b44b2ecae081c3713d73c4
CarolineRodrigues <31484196+CarolineRodrigues@users.noreply.github.com>
2022-11-25 17:36:58 +0000

    Radix ui accordion test

--- src/components/ui/accordion/accordion.tsx
+++ src/components/ui/accordion/accordion.tsx
@@ -1,3 +1,19 @@
+import * as AccordionRadixUi from '@radix-ui/react-accordion';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
+import './accordion.scss';
+
 export const Accordion = () => {
-  return <div className="text-hero">ola</div>;
+  return (
+    <AccordionRadixUi.Root className="border-b border-neutral-300 flex" type="single" collapsible>
+      <AccordionRadixUi.Item value="item-1">
+        <AccordionRadixUi.Header>
+          <AccordionRadixUi.Trigger className="AccordionTrigger flex">
+            <span>Trigger text</span>
+            <ChevronDownIcon className="AccordionChevron ml-16" aria-hidden />
+          </AccordionRadixUi.Trigger>
+        </AccordionRadixUi.Header>
+        <AccordionRadixUi.Content>…</AccordionRadixUi.Content>
+      </AccordionRadixUi.Item>
+    </AccordionRadixUi.Root>
+  );
 };
