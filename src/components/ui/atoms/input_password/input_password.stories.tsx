5ea8c268deaa4c9e1c4163a0d61d414cb4c72977
Maxwell Ramos <maxwell.ramos@zing.cv>
2023-02-01 14:37:47 +0000

    Input Password, revisão variada. Focus. Char Counter. Placeholder. Text Alignment

--- src/components/ui/atoms/input_password/input_password.stories.tsx
+++ src/components/ui/atoms/input_password/input_password.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentMeta, Story } from '@storybook/react';
 import React from 'react';
-import { InputPassword } from './input_password';
+import { InputPassword, InputPasswordProps } from './input_password';
 import { argTypes } from './input_password.csf';
 
 export default {
@@ -11,11 +11,17 @@ export default {
   }
 } as ComponentMeta<typeof InputPassword>;
 
-export const InputTextComp: Story<any> = (args) => <InputPassword {...args} id="input-password" />;
+export const InputTextComp: Story<any> = (args: InputPasswordProps) => (
+  <div className="p-8">
+    <InputPassword {...args} id="input-password" />
+  </div>
+);
 InputTextComp.storyName = 'Input Password';
 
-export const InputPasswordFeedbackInfo: Story<any> = (args) => (
-  <InputPassword {...args} id="input-password" feedbackText="Some info feedback" />
+export const InputPasswordFeedbackInfo: Story<any> = (args: InputPasswordProps) => (
+  <div className="p-8">
+    <InputPassword {...args} id="input-password" feedbackText="Some info feedback" />
+  </div>
 );
 InputPasswordFeedbackInfo.storyName = 'Input Password Feedback Info';
 InputPasswordFeedbackInfo.args = {
@@ -23,8 +29,10 @@ InputPasswordFeedbackInfo.args = {
   placeholder: 'Placeholder'
 };
 
-export const InputPasswordFeedbackSuccess: Story<any> = (args) => (
-  <InputPassword {...args} id="input-password" feedbackText="Some success feedback" feedbackState="success" />
+export const InputPasswordFeedbackSuccess: Story<any> = (args: InputPasswordProps) => (
+  <div className="p8">
+    <InputPassword {...args} id="input-password" feedbackText="Some success feedback" feedbackState="success" />
+  </div>
 );
 InputPasswordFeedbackSuccess.storyName = 'Input Password Feedback Success';
 InputPasswordFeedbackSuccess.args = {
@@ -32,8 +40,10 @@ InputPasswordFeedbackSuccess.args = {
   placeholder: 'Placeholder'
 };
 
-export const InputPasswordFeedbackWarning: Story<any> = (args) => (
-  <InputPassword {...args} id="input-password" feedbackText="Some warning feedback" feedbackState="warning" />
+export const InputPasswordFeedbackWarning: Story<any> = (args: InputPasswordProps) => (
+  <div className="p-8">
+    <InputPassword {...args} id="input-password" feedbackText="Some warning feedback" feedbackState="warning" />
+  </div>
 );
 InputPasswordFeedbackWarning.storyName = 'Input Password Feedback Warning';
 InputPasswordFeedbackWarning.args = {
@@ -41,8 +51,10 @@ InputPasswordFeedbackWarning.args = {
   placeholder: 'Placeholder'
 };
 
-export const InputPasswordFeedbackDanger: Story<any> = (args) => (
-  <InputPassword {...args} id="input-password" feedbackText="Some danger feedback" feedbackState="danger" />
+export const InputPasswordFeedbackDanger: Story<any> = (args: InputPasswordProps) => (
+  <div className="p-8">
+    <InputPassword {...args} id="input-password" feedbackText="Some danger feedback" feedbackState="danger" />
+  </div>
 );
 InputPasswordFeedbackDanger.storyName = 'Input Password Feedback Danger';
 InputPasswordFeedbackDanger.args = {
@@ -50,7 +62,11 @@ InputPasswordFeedbackDanger.args = {
   placeholder: 'Placeholder'
 };
 
-export const InputTextCompHasError: Story<any> = (args) => <InputPassword {...args} id="input-password" />;
+export const InputTextCompHasError: Story<any> = (args: InputPasswordProps) => (
+  <div className="p-8">
+    <InputPassword {...args} id="input-password" />
+  </div>
+);
 InputTextCompHasError.storyName = 'Input Password Error';
 InputTextCompHasError.args = {
   hasError: true,
@@ -58,7 +74,11 @@ InputTextCompHasError.args = {
   placeholder: 'Placeholder'
 };
 
-export const InputTextCompDisabled: Story<any> = (args) => <InputPassword {...args} id="input-password" />;
+export const InputTextCompDisabled: Story<any> = (args: InputPasswordProps) => (
+  <div className="p-8">
+    <InputPassword {...args} id="input-password" />
+  </div>
+);
 InputTextCompDisabled.storyName = 'Input Password Disabled';
 InputTextCompDisabled.args = {
   disabled: true,
@@ -66,7 +86,11 @@ InputTextCompDisabled.args = {
   placeholder: 'Placeholder'
 };
 
-export const InputTextCompHasReadOnly: Story<any> = (args) => <InputPassword {...args} id="input-password" />;
+export const InputTextCompHasReadOnly: Story<any> = (args: InputPasswordProps) => (
+  <div className="p-8">
+    <InputPassword {...args} id="input-password" />
+  </div>
+);
 InputTextCompHasReadOnly.storyName = 'Input Password Read Only';
 InputTextCompHasReadOnly.args = {
   readOnly: true,
